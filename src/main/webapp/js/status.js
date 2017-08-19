5933dde77b9d64ecb2216b8e502479c2539ffa7d
dernasherbrezon <rodionovamp@mail.ru>
2017-08-19 13:00:43 +0100

    load metrics only when tab activated

--- src/main/webapp/js/status.js
+++ src/main/webapp/js/status.js
@@ -1,43 +1,53 @@
 jQuery(document).ready(function($) {
-	var bytesGraphs = $(".rrdgraph-BYTES").rrd4j({
-	    legend: {
-	        show: false
-	    },
-	    yaxis: {
-	    	min: 0, 
-	    	mode: "byte" 
-	    },
-	    tooltip: {
-	    	show: true,
-	    	content: function() {
-	    		return "%y";
-	    	}
-	    }
-	}), normalGraphs = $(".rrdgraph-NORMAL").rrd4j({
-	    legend: {
-	        show: false
-	    },
-	    yaxis: {
-	    	min: 0, 
-	    },
-	    tooltip: {
-	    	show: true,
-	    	content: function() {
-	    		return "%y";
-	    	}
-	    }
-	});
-	$("#updateInterval").change(function(){
-		var option = $("option:selected", $(this)).val(), start = new Date();
-		if( option == "DAY" ) {
-			start.setDate(start.getDate() - 1);
-		} else if( option == "MONTH" ) {
-			start.setDate(start.getDate() - 31);
-		} else if( option == "YEAR" ) {
-			start.setDate(start.getDate() - 360);
+	
+	var loadedMetrics = false;
+
+	$(document).on('shown.bs.tab', '[href="#metrics"]', function (e) {
+		if( loadedMetrics ) {
+			return;
 		}
-		bytesGraphs.updateInterval(start.getTime(), Date.now());
-		normalGraphs.updateInterval(start.getTime(), Date.now());
+		var bytesGraphs = $(".rrdgraph-BYTES").rrd4j({
+			legend: {
+				show: false
+			},
+			yaxis: {
+				min: 0, 
+				mode: "byte" 
+			},
+			tooltip: {
+				show: true,
+				content: function() {
+					return "%y";
+				}
+			}
+		}), normalGraphs = $(".rrdgraph-NORMAL").rrd4j({
+			legend: {
+				show: false
+			},
+			yaxis: {
+				min: 0, 
+			},
+			tooltip: {
+				show: true,
+				content: function() {
+					return "%y";
+				}
+			}
+		});
+		$("#updateInterval").change(function(){
+			var option = $("option:selected", $(this)).val(), start = new Date();
+			if( option == "DAY" ) {
+				start.setDate(start.getDate() - 1);
+			} else if( option == "MONTH" ) {
+				start.setDate(start.getDate() - 31);
+			} else if( option == "YEAR" ) {
+				start.setDate(start.getDate() - 360);
+			}
+			bytesGraphs.updateInterval(start.getTime(), Date.now());
+			normalGraphs.updateInterval(start.getTime(), Date.now());
+		});
+		
+		loadedMetrics = true;
 	});
 	
     var dashboard = document.getElementById("dashboard");
