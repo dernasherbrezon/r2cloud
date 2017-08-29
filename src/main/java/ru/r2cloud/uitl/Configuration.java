cdf5de52b0cfcdd7352267b3884a0e5a850ed629
dernasherbrezon <rodionovamp@mail.ru>
2017-08-29 19:30:15 +0100

    properly secure file with passwords

--- src/main/java/ru/r2cloud/uitl/Configuration.java
+++ src/main/java/ru/r2cloud/uitl/Configuration.java
@@ -5,12 +5,26 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Logger;
 
 public class Configuration extends Properties {
 
+	private static final Logger LOG = Logger.getLogger(Configuration.class.getName());
 	private static final long serialVersionUID = 4175467887601528587L;
 	private String propertiesLocation;
+
+	private static Set<PosixFilePermission> MODE600 = new HashSet<PosixFilePermission>();
+
+	static {
+		MODE600.add(PosixFilePermission.OWNER_READ);
+		MODE600.add(PosixFilePermission.OWNER_WRITE);
+	}
 
 	public Configuration(String propertiesLocation) {
 		try (InputStream is = new FileInputStream(propertiesLocation)) {
@@ -33,6 +47,11 @@ public class Configuration extends Properties {
 			store(fos, "updated");
 		} catch (IOException e) {
 			throw new RuntimeException(e);
+		}
+		try {
+			Files.setPosixFilePermissions(Paths.get(propertiesLocation), MODE600);
+		} catch (IOException e) {
+			LOG.info("unable to setup 600 permissions: " + e.getMessage());
 		}
 	}
 
