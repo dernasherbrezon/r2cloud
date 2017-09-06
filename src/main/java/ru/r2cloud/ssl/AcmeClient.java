7d94e66fa40749ede8595e797fcc90bf2db13775
dernasherbrezon <rodionovamp@mail.ru>
2017-09-06 21:47:33 +0100

    renew in acmeclient

--- src/main/java/ru/r2cloud/ssl/AcmeClient.java
+++ src/main/java/ru/r2cloud/ssl/AcmeClient.java
@@ -1,6 +1,7 @@
 package ru.r2cloud.ssl;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -11,9 +12,11 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,6 +33,7 @@ import org.shredzone.acme4j.challenge.Http01Challenge;
 import org.shredzone.acme4j.exception.AcmeConflictException;
 import org.shredzone.acme4j.exception.AcmeException;
 import org.shredzone.acme4j.exception.AcmeRetryAfterException;
+import org.shredzone.acme4j.exception.AcmeUnauthorizedException;
 import org.shredzone.acme4j.util.CSRBuilder;
 import org.shredzone.acme4j.util.CertificateUtils;
 import org.shredzone.acme4j.util.KeyPairUtils;
@@ -59,8 +63,75 @@ public class AcmeClient {
 
 	public void start() {
 		executor = Executors.newSingleThreadScheduledExecutor(new NamingThreadFactory("acme-client"));
-		// FIXME check new File(basepath, "domain-chain.crt") exists and
-		// schedule re-new
+		if (isSSLEnabled()) {
+			try (FileInputStream fis = new FileInputStream(new File(basepath, "domain-chain.crt"))) {
+				X509Certificate certificate = CertificateUtils.readX509Certificate(fis);
+				scheduleRenew(certificate);
+			} catch (IOException e) {
+				LOG.log(Level.SEVERE, "unable to load certificate for renewal", e);
+			}
+		}
+	}
+
+	private void scheduleRenew(X509Certificate certificate) {
+		long delay = certificate.getNotAfter().getTime() - System.currentTimeMillis() - TimeUnit.DAYS.toMillis(7);
+		messages.add("Schedule certificate renewal. NotAfter: " + certificate.getNotAfter() + " Renew at: " + new Date(certificate.getNotAfter().getTime() - TimeUnit.DAYS.toMillis(7)), LOG);
+		executor.schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!running.compareAndSet(false, true)) {
+					LOG.info("acmeclient is already running. skip renew");
+					return;
+				}
+				messages.clear();
+				
+				Registration reg = loadOrCreateRegistration();
+				if (reg == null) {
+					return;
+				}
+				byte[] csr;
+				try (FileInputStream fis = new FileInputStream(new File(basepath, "domain.csr"))) {
+					csr = CertificateUtils.readCSR(fis).getEncoded();
+				} catch (Exception e) {
+					LOG.log(Level.SEVERE, "unable to load csr. trying to create new", e);
+					CSRBuilder csrb = createCSR(reg);
+					if (csrb == null) {
+						return;
+					}
+					try {
+						csr = csrb.getEncoded();
+					} catch (IOException e1) {
+						LOG.log(Level.SEVERE, "unable to encode csr", e1);
+						return;
+					}
+				}
+
+				Certificate certificate;
+				try {
+					certificate = reg.requestCertificate(csr);
+				} catch (AcmeUnauthorizedException e) {
+					LOG.info("authorizations expired. create new csr");
+					CSRBuilder csrb = createCSR(reg);
+					if (csrb == null) {
+						return;
+					}
+					try {
+						certificate = reg.requestCertificate(csrb.getEncoded());
+					} catch (Exception e1) {
+						LOG.log(Level.SEVERE, "unable to renew certificate with new csr", e1);
+						return;
+					}
+				} catch (AcmeException e) {
+					LOG.log(Level.SEVERE, "unable to renew certificate", e);
+					return;
+				}
+				
+				downloadCertificate(certificate);
+				
+				running.set(false);
+			}
+		}, delay, TimeUnit.MILLISECONDS);
 	}
 
 	public void setup() {
@@ -74,75 +145,13 @@ public class AcmeClient {
 			public void run() {
 				messages.add("starting up...", LOG);
 
-				KeyPair userKeyPair;
-				try {
-					userKeyPair = loadOrCreateKeyPair(new File(basepath, "user.key"));
-				} catch (IOException e) {
-					String message = "unable to load user keypair";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
+				Registration reg = loadOrCreateRegistration();
+				if (reg == null) {
 					return;
 				}
 
-				messages.add("creating session", LOG);
-				Session session = new Session(config.getProperty("acme.ca.url"), userKeyPair);
-
-				Registration reg;
-				try {
-					reg = findOrRegisterAccount(session);
-				} catch (AcmeException e) {
-					String message = "unable to setup account";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
-
-				String domain = config.getProperty("ddns.noip.domain");
-				try {
-					authorize(reg, domain);
-				} catch (Exception e) {
-					String message = "unable to authorize domain";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				} finally {
-					messages.add("cleanup challenge data", LOG);
-					for (File cur : challengePath.listFiles()) {
-						if (cur.isFile() && !cur.delete()) {
-							LOG.info("unable to cleanup: " + cur.getAbsolutePath());
-						}
-					}
-				}
-
-				KeyPair domainKeyPair;
-				try {
-					domainKeyPair = loadOrCreateKeyPair(new File(basepath, "domain.key"));
-				} catch (IOException e) {
-					String message = "unable to load domain keypair";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
-
-				messages.add("creating csr", LOG);
-				CSRBuilder csrb = new CSRBuilder();
-				csrb.addDomain(domain);
-				try {
-					csrb.sign(domainKeyPair);
-				} catch (IOException e) {
-					String message = "unable to sign csr";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
-
-				messages.add("saving csr for future use", LOG);
-				try (Writer out = new FileWriter(new File(basepath, "domain.csr"))) {
-					csrb.write(out);
-				} catch (IOException e) {
-					String message = "unable to save csr";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
+				CSRBuilder csrb = createCSR(reg);
+				if (csrb == null) {
 					return;
 				}
 
@@ -157,43 +166,124 @@ public class AcmeClient {
 					return;
 				}
 
-				messages.add("downloading certificate", LOG);
-				X509Certificate cert;
-				try {
-					cert = certificate.download();
-				} catch (AcmeException e) {
-					String message = "unable to download certificate";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
-				messages.add("downloading certificate chain", LOG);
-				X509Certificate[] chain;
-				try {
-					chain = certificate.downloadChain();
-				} catch (AcmeException e) {
-					String message = "unable to download certificate chain";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
-				messages.add("saving certificate", LOG);
-				try (FileWriter fw = new FileWriter(new File(basepath, "domain-chain.crt"))) {
-					CertificateUtils.writeX509CertificateChain(fw, cert, chain);
-				} catch (IOException e) {
-					String message = "unable to save certificate";
-					messages.add(message);
-					LOG.log(Level.SEVERE, message, e);
-					return;
-				}
+				downloadCertificate(certificate);
 
 				running.set(false);
 			}
 		});
 	}
+	
+	private void downloadCertificate(Certificate certificate) {
+		messages.add("downloading certificate", LOG);
+		X509Certificate cert;
+		try {
+			cert = certificate.download();
+		} catch (AcmeException e) {
+			String message = "unable to download certificate";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return;
+		}
+		
+		scheduleRenew(cert);
+		
+		messages.add("downloading certificate chain", LOG);
+		X509Certificate[] chain;
+		try {
+			chain = certificate.downloadChain();
+		} catch (AcmeException e) {
+			String message = "unable to download certificate chain";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return;
+		}
+		messages.add("saving certificate", LOG);
+		try (FileWriter fw = new FileWriter(new File(basepath, "domain-chain.crt"))) {
+			CertificateUtils.writeX509CertificateChain(fw, cert, chain);
+		} catch (IOException e) {
+			String message = "unable to save certificate";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return;
+		}
+	}
 
-	public List<String> getMessages() {
-		return messages.get();
+	private CSRBuilder createCSR(Registration reg) {
+		String domain = config.getProperty("ddns.noip.domain");
+		try {
+			authorize(reg, domain);
+		} catch (Exception e) {
+			String message = "unable to authorize domain";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		} finally {
+			messages.add("cleanup challenge data", LOG);
+			for (File cur : challengePath.listFiles()) {
+				if (cur.isFile() && !cur.delete()) {
+					LOG.info("unable to cleanup: " + cur.getAbsolutePath());
+				}
+			}
+		}
+
+		KeyPair domainKeyPair;
+		try {
+			domainKeyPair = loadOrCreateKeyPair(new File(basepath, "domain.key"));
+		} catch (IOException e) {
+			String message = "unable to load domain keypair";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		}
+
+		messages.add("creating csr", LOG);
+		CSRBuilder csrb = new CSRBuilder();
+		csrb.addDomain(domain);
+		try {
+			csrb.sign(domainKeyPair);
+		} catch (IOException e) {
+			String message = "unable to sign csr";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		}
+
+		messages.add("saving csr for future use", LOG);
+		try (Writer out = new FileWriter(new File(basepath, "domain.csr"))) {
+			csrb.write(out);
+		} catch (IOException e) {
+			String message = "unable to save csr";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		}
+		return csrb;
+	}
+
+	private Registration loadOrCreateRegistration() {
+		KeyPair userKeyPair;
+		try {
+			userKeyPair = loadOrCreateKeyPair(new File(basepath, "user.key"));
+		} catch (IOException e) {
+			String message = "unable to load user keypair";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		}
+
+		messages.add("creating session", LOG);
+		Session session = new Session(config.getProperty("acme.ca.url"), userKeyPair);
+
+		Registration reg;
+		try {
+			reg = loadOrRegisterAccount(session);
+		} catch (AcmeException e) {
+			String message = "unable to setup account";
+			messages.add(message);
+			LOG.log(Level.SEVERE, message, e);
+			return null;
+		}
+		return reg;
 	}
 
 	private KeyPair loadOrCreateKeyPair(File file) throws IOException {
@@ -212,7 +302,7 @@ public class AcmeClient {
 		}
 	}
 
-	private Registration findOrRegisterAccount(Session session) throws AcmeException {
+	private Registration loadOrRegisterAccount(Session session) throws AcmeException {
 		Registration reg;
 		try {
 			messages.add("registering new user", LOG);
@@ -234,8 +324,17 @@ public class AcmeClient {
 	private void authorize(Registration reg, String domain) throws AcmeException, FileNotFoundException, IOException {
 		messages.add("authorizing domain: " + domain, LOG);
 		Authorization auth = reg.authorizeDomain(domain);
+		messages.add("find http challenge", LOG);
+		Http01Challenge challenge1 = auth.findChallenge(Http01Challenge.TYPE);
+		if (challenge1 == null) {
+			throw new AcmeException("Found no " + Http01Challenge.TYPE + " challenge, don't know what to do...");
+		}
+		messages.add("saving challenge request", LOG);
+		try (FileOutputStream fos = new FileOutputStream(new File(challengePath, challenge1.getToken()))) {
+			fos.write(challenge1.getAuthorization().getBytes(StandardCharsets.UTF_8));
+		}
 
-		Challenge challenge = httpChallenge(auth);
+		Challenge challenge = challenge1;
 		if (challenge.getStatus() == Status.VALID) {
 			messages.add("challenge already successeded", LOG);
 			return;
@@ -272,19 +371,6 @@ public class AcmeClient {
 		}
 	}
 
-	public Challenge httpChallenge(Authorization auth) throws AcmeException, FileNotFoundException, IOException {
-		messages.add("find http challenge", LOG);
-		Http01Challenge challenge = auth.findChallenge(Http01Challenge.TYPE);
-		if (challenge == null) {
-			throw new AcmeException("Found no " + Http01Challenge.TYPE + " challenge, don't know what to do...");
-		}
-		messages.add("saving challenge request", LOG);
-		try (FileOutputStream fos = new FileOutputStream(new File(challengePath, challenge.getToken()))) {
-			fos.write(challenge.getAuthorization().getBytes(StandardCharsets.UTF_8));
-		}
-		return challenge;
-	}
-
 	public void stop() {
 		if (executor != null) {
 			executor.shutdown();
@@ -299,4 +385,7 @@ public class AcmeClient {
 		return running.get();
 	}
 
+	public List<String> getMessages() {
+		return messages.get();
+	}
 }
