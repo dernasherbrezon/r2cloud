11ca179ebc9f742ab252961f9d5f1c1cb6e23dd0
dernasherbrezon <rodionovamp@mail.ru>
2017-10-31 22:05:19 +0000

    fix repo

--- Dockerbuild/test.sh
+++ Dockerbuild/test.sh
@@ -3,7 +3,12 @@
 set -e
 set -o pipefail
 
-apt-get update 
-dpkg -i *.deb
+sudo gpg --keyserver keyserver.ubuntu.com --recv-keys 129E7CDE
+sudo gpg --armor --export 129E7CDE | sudo apt-key add -
+sudo bash -c "echo 'deb [arch=armhf] http://s3.amazonaws.com/r2cloud r2cloud main' > /etc/apt/sources.list.d/r2cloud.list"
+
+apt-get update
+ 
+apt install ./*.deb
 
 echo "test success"
