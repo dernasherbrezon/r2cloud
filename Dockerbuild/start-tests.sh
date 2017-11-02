b015bc8d84f93b58d2fd794813cc5bbbdbf3941e
dernasherbrezon <rodionovamp@mail.ru>
2017-11-02 16:29:30 +0000

    run tests from normal openjdk distribution

--- Dockerbuild/start-tests.sh
+++ Dockerbuild/start-tests.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-/home/pi/ejdk-8-compact1/bin/java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud/lib/* -Duser.timezone=UTC -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.IntegrationalTests
+java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud/lib/* -Duser.timezone=UTC -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.IntegrationalTests
