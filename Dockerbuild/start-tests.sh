94c2c838667bbe0e94f8e3e247b2998bef16b454
dernasherbrezon <rodionovamp@mail.ru>
2017-11-10 10:27:03 +0000

    fix it tests

--- Dockerbuild/start-tests.sh
+++ Dockerbuild/start-tests.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud/lib/* -Duser.timezone=UTC -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.it.IntegrationalTests
+java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud/lib/*:/usr/share/java/r2cloud/* -Duser.timezone=UTC -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.it.IntegrationalTests
