2803f9113e8abef4d2f53a2f21658a11b9d3ddc0
dernasherbrezon <rodionovamp@mail.ru>
2017-11-30 16:52:20 +0000

    configure ssl

--- src/main/java/ru/r2cloud/web/api/configuration/SSLLog.java
+++ src/main/java/ru/r2cloud/web/api/configuration/SSLLog.java
@@ -1,47 +1,42 @@
-package ru.r2cloud.web.controller;
+package ru.r2cloud.web.api.configuration;
 
 import java.util.List;
 
-import ru.r2cloud.ssl.AcmeClient;
-import ru.r2cloud.web.AbstractHttpController;
-import ru.r2cloud.web.MimeType;
-import ru.r2cloud.web.ModelAndView;
-import ru.r2cloud.web.WebServer;
-
-import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
+import ru.r2cloud.ssl.AcmeClient;
+import ru.r2cloud.web.AbstractHttpController;
+import ru.r2cloud.web.ModelAndView;
+import ru.r2cloud.web.WebServer;
 
-public class GetAcmeLog extends AbstractHttpController {
+public class SSLLog extends AbstractHttpController {
 
 	private final AcmeClient client;
 
-	public GetAcmeLog(AcmeClient client) {
+	public SSLLog(AcmeClient client) {
 		this.client = client;
 	}
 
 	@Override
 	public ModelAndView doGet(IHTTPSession session) {
 		ModelAndView result = new ModelAndView();
-		//FIXME
-		//result.setType(MimeType.JSON);
 		if (client.isRunning()) {
 			result.setStatus(Response.Status.PARTIAL_CONTENT);
 		}
-		JsonArray array = (JsonArray) Json.array();
+		JsonArray array = new JsonArray();
 		int index = WebServer.getInteger(session, "index");
 		List<String> messages = client.getMessages();
 		for (int i = index; i < messages.size(); i++) {
 			array.add(messages.get(i));
 		}
-		result.put("entity", array.toString());
+		result.setData(array.toString());
 		return result;
 	}
 
 	@Override
 	public String getRequestMappingURL() {
-		return "/admin/config/ssl/log.json";
+		return "/api/v1/admin/config/ssl/log";
 	}
 }
