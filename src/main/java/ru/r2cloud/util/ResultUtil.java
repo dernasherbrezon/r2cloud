b1c4f8171182df7582e7d50571e3eed5c6f1325a
dernasherbrezon <rodionovamp@mail.ru>
2019-03-29 16:43:46 +0000

    fix code smells

--- src/main/java/ru/r2cloud/util/ResultUtil.java
+++ src/main/java/ru/r2cloud/util/ResultUtil.java
@@ -7,21 +7,23 @@ import com.codahale.metrics.health.HealthCheck.ResultBuilder;
 
 public final class ResultUtil {
 
+	private static final String FIELD_NAME = "status";
+
 	public static Result unhealthy(String message) {
 		ResultBuilder builder = Result.builder();
-		builder.unhealthy().withMessage(message).withDetail("status", Status.ERROR);
+		builder.unhealthy().withMessage(message).withDetail(FIELD_NAME, Status.ERROR);
 		return builder.build();
 	}
 	
 	public static Result healthy() {
 		ResultBuilder builder = Result.builder();
-		builder.healthy().withDetail("status", Status.SUCCESS);
+		builder.healthy().withDetail(FIELD_NAME, Status.SUCCESS);
 		return builder.build();
 	}
 	
 	public static Result unknown() {
 		ResultBuilder builder = Result.builder();
-		builder.healthy().withDetail("status", Status.UNKNOWN);
+		builder.healthy().withDetail(FIELD_NAME, Status.UNKNOWN);
 		return builder.build();
 	}
 
