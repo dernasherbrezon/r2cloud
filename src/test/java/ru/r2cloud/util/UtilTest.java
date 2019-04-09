fecac668ffe794596b2b36430b3f65616055fec7
dernasherbrezon <rodionovamp@mail.ru>
2019-04-09 21:55:25 +0100

    add util test

---
+++ src/test/java/ru/r2cloud/util/UtilTest.java
@@ -0,0 +1,32 @@
+package ru.r2cloud.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import java.util.zip.GZIPOutputStream;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class UtilTest {
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	@Test
+	public void testTotalSamples() throws Exception {
+		long totalSamplesExpected = 50;
+		File wav = new File(tempFolder.getRoot(), UUID.randomUUID().toString());
+		try (OutputStream fos = new GZIPOutputStream(new FileOutputStream(wav))) {
+			for (int i = 0; i < totalSamplesExpected; i++) {
+				fos.write(0x01);
+			}
+		}
+		assertEquals(totalSamplesExpected / 2, Util.readTotalSamples(wav).longValue());
+	}
+
+}
