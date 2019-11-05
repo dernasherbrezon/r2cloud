59d448e07e01b3fccab505d4bd68c717391887f5
dernasherbrezon <rodionovamp@mail.ru>
2019-11-05 07:23:04 +0000

    remove unused

--- src/main/java/ru/r2cloud/web/ModelAndView.java
+++ src/main/java/ru/r2cloud/web/ModelAndView.java
@@ -1,6 +1,5 @@
 package ru.r2cloud.web;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.eclipsesource.json.JsonValue;
@@ -23,17 +22,6 @@ public class ModelAndView {
 
 	public Map<String, String> getHeaders() {
 		return headers;
-	}
-
-	public void setHeaders(Map<String, String> headers) {
-		this.headers = headers;
-	}
-
-	public void addHeader(String name, String value) {
-		if (headers == null) {
-			headers = new HashMap<String, String>();
-		}
-		headers.put(name, value);
 	}
 
 	public String getData() {
