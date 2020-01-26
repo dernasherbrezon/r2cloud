8cc13437803355d98ba0b739a65568bad5ea873b
dernasherbrezon <rodionovamp@mail.ru>
2020-01-26 10:06:36 +0000

    add test for jy1sat decoder

---
+++ src/test/java/ru/r2cloud/satellite/decoder/Jy1satDecoderTest.java
@@ -0,0 +1,41 @@
+package ru.r2cloud.satellite.decoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import ru.r2cloud.TestConfiguration;
+import ru.r2cloud.TestUtil;
+import ru.r2cloud.model.ObservationResult;
+
+public class Jy1satDecoderTest {
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private TestConfiguration config;
+
+	@Test
+	public void testSomeData() throws Exception {
+		File wav = TestUtil.setupClasspathResource(tempFolder, "data/jy1sat.raw.gz");
+		Jy1satDecoder decoder = new Jy1satDecoder(config);
+		ObservationResult result = decoder.decode(wav, TestUtil.loadObservation("data/jy1sat.raw.gz.json").getReq());
+		assertEquals(1, result.getNumberOfDecodedPackets().longValue());
+		assertNotNull(result.getDataPath());
+		assertNotNull(result.getIqPath());
+	}
+
+	@Before
+	public void start() throws Exception {
+		config = new TestConfiguration(tempFolder);
+		config.setProperty("server.tmp.directory", tempFolder.getRoot().getAbsolutePath());
+		config.update();
+	}
+
+}
