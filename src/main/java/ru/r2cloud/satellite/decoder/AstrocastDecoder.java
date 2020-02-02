b5ac1e5ed2cde553085ec57326d92f85c4a5ebc6
dernasherbrezon <rodionovamp@mail.ru>
2020-02-02 09:22:58 +0100

    test for astrocast decoder

--- src/main/java/ru/r2cloud/satellite/decoder/AstrocastDecoder.java
+++ src/main/java/ru/r2cloud/satellite/decoder/AstrocastDecoder.java
@@ -2,14 +2,13 @@ package ru.r2cloud.satellite.decoder;
 
 import ru.r2cloud.jradio.Beacon;
 import ru.r2cloud.jradio.BeaconSource;
-import ru.r2cloud.jradio.Endianness;
 import ru.r2cloud.jradio.FloatInput;
-import ru.r2cloud.jradio.astrocast.Astrocast;
-import ru.r2cloud.jradio.astrocast.AstrocastBeacon;
+import ru.r2cloud.jradio.astrocast.Astrocast9k6;
+import ru.r2cloud.jradio.astrocast.Astrocast9k6Beacon;
 import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
 import ru.r2cloud.jradio.blocks.FixedLengthTagger;
+import ru.r2cloud.jradio.blocks.SoftToHard;
 import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
-import ru.r2cloud.jradio.blocks.UnpackedToPacked;
 import ru.r2cloud.model.ObservationRequest;
 import ru.r2cloud.util.Configuration;
 
@@ -22,16 +21,17 @@ public class AstrocastDecoder extends TelemetryDecoder {
 	@Override
 	public BeaconSource<? extends Beacon> createBeaconSource(FloatInput source, ObservationRequest req) {
 		float gainMu = 0.175f * 5;
-		int baud = 1200;
+		int baud = 9600;
 		GmskDemodulator gmsk = new GmskDemodulator(source, baud, req.getBandwidth(), gainMu);
-		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(gmsk, 8, "0111010111111010110000011010001101011000110100000110010001110110", false);
-		TaggedStreamToPdu pdu = new TaggedStreamToPdu(new UnpackedToPacked(new FixedLengthTagger(correlateTag, 255 * 8), 1, Endianness.GR_MSB_FIRST));
-		return new Astrocast(pdu);
+		SoftToHard s2h = new SoftToHard(gmsk);
+		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(s2h, 4, "00011010110011111111110000011101", false);
+		TaggedStreamToPdu pdu = new TaggedStreamToPdu(new FixedLengthTagger(correlateTag, 255 * 8 * 5));
+		return new Astrocast9k6(pdu);
 	}
-	
+
 	@Override
 	public Class<? extends Beacon> getBeaconClass() {
-		return AstrocastBeacon.class;
+		return Astrocast9k6Beacon.class;
 	}
 
 }
