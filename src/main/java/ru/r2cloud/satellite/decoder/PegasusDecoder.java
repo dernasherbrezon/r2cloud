9442b9a8a0c0a7961ef9ba2131aa5ac912a1f585
dernasherbrezon <rodionovamp@mail.ru>
2020-02-05 19:55:00 +0100

    fix pegasus decoder + tests

--- src/main/java/ru/r2cloud/satellite/decoder/PegasusDecoder.java
+++ src/main/java/ru/r2cloud/satellite/decoder/PegasusDecoder.java
@@ -8,6 +8,7 @@ import ru.r2cloud.jradio.at03.At03;
 import ru.r2cloud.jradio.at03.At03Beacon;
 import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
 import ru.r2cloud.jradio.blocks.FixedLengthTagger;
+import ru.r2cloud.jradio.blocks.SoftToHard;
 import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
 import ru.r2cloud.jradio.blocks.UnpackedToPacked;
 import ru.r2cloud.model.ObservationRequest;
@@ -21,9 +22,10 @@ public class PegasusDecoder extends TelemetryDecoder {
 
 	@Override
 	public BeaconSource<? extends Beacon> createBeaconSource(FloatInput source, ObservationRequest req) {
-		float gainMu = 0.175f;
+		float gainMu = 0.175f * 3;
 		GmskDemodulator gmsk = new GmskDemodulator(source, 9600, req.getBandwidth(), gainMu);
-		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(gmsk, 1, "0010110111010100", false);
+		SoftToHard s2h = new SoftToHard(gmsk);
+		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(s2h, 2, "0010110111010100", false);
 		TaggedStreamToPdu pdu = new TaggedStreamToPdu(new UnpackedToPacked(new FixedLengthTagger(correlateTag, 64 * 8), 1, Endianness.GR_MSB_FIRST));
 		return new At03(pdu);
 	}
