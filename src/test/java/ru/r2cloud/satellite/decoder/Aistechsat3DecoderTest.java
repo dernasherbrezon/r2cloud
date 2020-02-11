1ef6c3e387209d595fadb1692732aee6ffb54c6d
dernasherbrezon <rodionovamp@mail.ru>
2020-02-11 07:18:29 +0000

    test for aistechsat3 decoder

---
+++ src/test/java/ru/r2cloud/satellite/decoder/Aistechsat3DecoderTest.java
@@ -0,0 +1,41 @@
+package ru.r2cloud.satellite.decoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import ru.r2cloud.TestConfiguration;
+import ru.r2cloud.TestUtil;
+import ru.r2cloud.model.ObservationResult;
+
+public class Aistechsat3DecoderTest {
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private TestConfiguration config;
+
+	@Test
+	public void testSomeData() throws Exception {
+		File wav = TestUtil.setupClasspathResource(tempFolder, "data/aistechsat3.raw.gz");
+		Aistechsat3Decoder decoder = new Aistechsat3Decoder(config);
+		ObservationResult result = decoder.decode(wav, TestUtil.loadObservation("data/aistechsat3.raw.gz.json").getReq());
+		assertEquals(1, result.getNumberOfDecodedPackets().longValue());
+		assertNotNull(result.getDataPath());
+		assertNotNull(result.getIqPath());
+	}
+
+	@Before
+	public void start() throws Exception {
+		config = new TestConfiguration(tempFolder);
+		config.setProperty("server.tmp.directory", tempFolder.getRoot().getAbsolutePath());
+		config.update();
+	}
+
+}
