d51701e6a48cf7ae3994a8d80a6a98370664d52d
dernasherbrezon <rodionovamp@mail.ru>
2020-04-09 16:43:54 +0100

    add test for swampsat-2

---
+++ src/test/java/ru/r2cloud/satellite/decoder/Swampsat2DecoderTest.java
@@ -0,0 +1,43 @@
+package ru.r2cloud.satellite.decoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import ru.r2cloud.TestConfiguration;
+import ru.r2cloud.TestUtil;
+import ru.r2cloud.model.ObservationResult;
+import ru.r2cloud.predict.PredictOreKit;
+
+public class Swampsat2DecoderTest {
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private TestConfiguration config;
+
+	@Test
+	public void testSomeData() throws Exception {
+		File wav = TestUtil.setupClasspathResource(tempFolder, "data/swampsat2.raw.gz");
+		PredictOreKit predict = new PredictOreKit(config);
+		Swampsat2Decoder decoder = new Swampsat2Decoder(predict, config);
+		ObservationResult result = decoder.decode(wav, TestUtil.loadObservation("data/swampsat2.raw.gz.json").getReq());
+		assertEquals(1, result.getNumberOfDecodedPackets().longValue());
+		assertNotNull(result.getDataPath());
+		assertNotNull(result.getIqPath());
+	}
+
+	@Before
+	public void start() throws Exception {
+		config = new TestConfiguration(tempFolder);
+		config.setProperty("server.tmp.directory", tempFolder.getRoot().getAbsolutePath());
+		config.update();
+	}
+
+}
