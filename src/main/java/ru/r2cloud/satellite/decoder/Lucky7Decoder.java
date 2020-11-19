0e82423b341cf810f376e6074a720506b5aeb212
dernasherbrezon <rodionovamp@mail.ru>
2020-11-19 21:46:56 +0000

    add support for lucky7 images

--- src/main/java/ru/r2cloud/satellite/decoder/Lucky7Decoder.java
+++ src/main/java/ru/r2cloud/satellite/decoder/Lucky7Decoder.java
@@ -1,6 +1,20 @@
 package ru.r2cloud.satellite.decoder;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ru.r2cloud.jradio.Beacon;
+import ru.r2cloud.jradio.BeaconInputStream;
 import ru.r2cloud.jradio.BeaconSource;
 import ru.r2cloud.jradio.FloatInput;
 import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
@@ -9,14 +23,54 @@ import ru.r2cloud.jradio.blocks.SoftToHard;
 import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
 import ru.r2cloud.jradio.lucky7.Lucky7;
 import ru.r2cloud.jradio.lucky7.Lucky7Beacon;
+import ru.r2cloud.jradio.lucky7.Lucky7PictureDecoder;
+import ru.r2cloud.model.DecoderResult;
 import ru.r2cloud.model.ObservationRequest;
 import ru.r2cloud.predict.PredictOreKit;
 import ru.r2cloud.util.Configuration;
 
 public class Lucky7Decoder extends TelemetryDecoder {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Lucky7Decoder.class);
+
 	public Lucky7Decoder(PredictOreKit predict, Configuration config) {
 		super(predict, config);
+	}
+
+	@Override
+	public DecoderResult decode(File rawIq, ObservationRequest req) {
+		DecoderResult result = super.decode(rawIq, req);
+		if (result.getDataPath() != null) {
+			List<Lucky7Beacon> beacons = new ArrayList<>();
+			try (BeaconInputStream<Lucky7Beacon> bis = new BeaconInputStream<>(new BufferedInputStream(new FileInputStream(result.getDataPath())), Lucky7Beacon.class)) {
+				while (bis.hasNext()) {
+					beacons.add(bis.next());
+				}
+			} catch (IOException e) {
+				LOG.error("unable to read data", e);
+			}
+			Lucky7PictureDecoder decoder = new Lucky7PictureDecoder(beacons);
+			while (decoder.hasNext()) {
+				File imageFile = saveImage("lucky7-" + req.getId() + ".jpg", decoder.next());
+				if (imageFile != null) {
+					result.setImagePath(imageFile);
+					// interested only in the first image
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+	private File saveImage(String path, BufferedImage image) {
+		File imageFile = new File(config.getTempDirectory(), path);
+		try {
+			ImageIO.write(image, "jpg", imageFile);
+			return imageFile;
+		} catch (IOException e) {
+			LOG.error("unable to write image", e);
+			return null;
+		}
 	}
 
 	@Override
