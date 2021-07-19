b8589e2ec8b42172f0ccf730ab7cf7dde6b4c0cf
dernasherbrezon <rodionovamp@mail.ru>
2021-07-19 17:16:05 +0100

    add tubin test + improve technosat decoder

--- src/main/java/ru/r2cloud/satellite/decoder/TechnosatDecoder.java
+++ src/main/java/ru/r2cloud/satellite/decoder/TechnosatDecoder.java
@@ -4,6 +4,8 @@ import ru.r2cloud.jradio.Beacon;
 import ru.r2cloud.jradio.BeaconSource;
 import ru.r2cloud.jradio.FloatInput;
 import ru.r2cloud.jradio.blocks.CorrelateSyncword;
+import ru.r2cloud.jradio.blocks.InvertBits;
+import ru.r2cloud.jradio.blocks.SoftToHard;
 import ru.r2cloud.jradio.demod.FskDemodulator;
 import ru.r2cloud.jradio.technosat.Technosat;
 import ru.r2cloud.jradio.technosat.TechnosatBeacon;
@@ -21,8 +23,10 @@ public class TechnosatDecoder extends TelemetryDecoder {
 
 	@Override
 	public BeaconSource<? extends Beacon> createBeaconSource(FloatInput source, ObservationRequest req) {
-		FskDemodulator demod = new FskDemodulator(source, 4800, 5000.0f, Util.convertDecimation(4800), 2000, true);
-		CorrelateSyncword correlateTag = new CorrelateSyncword(demod, 4, "111011110000111011110000", CMX909bBeacon.MAX_SIZE * 8);
+		FskDemodulator demod = new FskDemodulator(source, 4800, 2400.0f, Util.convertDecimation(4800), 2000, true);
+		SoftToHard s2h = new SoftToHard(demod);
+		InvertBits invert = new InvertBits(s2h);		
+		CorrelateSyncword correlateTag = new CorrelateSyncword(invert, 4, "111011110000111011110000", CMX909bBeacon.MAX_SIZE * 8);
 		return new Technosat(correlateTag);
 	}
 	
