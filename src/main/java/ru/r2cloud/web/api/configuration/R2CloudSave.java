4f5010fe0d743808204f39a1421001f1582bb82e
dernasherbrezon <rodionovamp@mail.ru>
2021-08-13 19:58:47 +0300

    configure new launches parameter in UI

--- src/main/java/ru/r2cloud/web/api/configuration/R2CloudSave.java
+++ src/main/java/ru/r2cloud/web/api/configuration/R2CloudSave.java
@@ -18,6 +18,7 @@ public class R2CloudSave extends AbstractHttpController {
 
 	private static final String APIKEY_PARAMETER = "apiKey";
 	private static final String SYNC_PARAMETER = "syncSpectogram";
+	private static final String NEW_LAUNCH_PARAMETER = "newLaunch";
 
 	private static final Logger LOG = LoggerFactory.getLogger(R2CloudSave.class);
 
@@ -33,6 +34,7 @@ public class R2CloudSave extends AbstractHttpController {
 		JsonObject entity = new JsonObject();
 		entity.add(APIKEY_PARAMETER, config.getProperty("r2cloud.apiKey"));
 		entity.add(SYNC_PARAMETER, config.getBoolean("r2cloud.syncSpectogram"));
+		entity.add(NEW_LAUNCH_PARAMETER, config.getBoolean("r2cloud.newLaunches"));
 		result.setData(entity.toString());
 		return result;
 	}
@@ -41,16 +43,18 @@ public class R2CloudSave extends AbstractHttpController {
 	public ModelAndView doPost(JsonObject request) {
 		ValidationResult errors = new ValidationResult();
 		String apiKey = WebServer.getString(request, APIKEY_PARAMETER);
-		boolean syncSpectogram = WebServer.getBoolean(request, SYNC_PARAMETER);
 		if (apiKey == null || apiKey.trim().length() == 0) {
 			errors.put(APIKEY_PARAMETER, "Cannot be empty");
 		}
+		boolean syncSpectogram = WebServer.getBoolean(request, SYNC_PARAMETER);
+		boolean newLaunches = WebServer.getBoolean(request, NEW_LAUNCH_PARAMETER);
 		if (!errors.isEmpty()) {
 			LOG.info("unable to save: {}", errors);
 			return new BadRequest(errors);
 		}
 		config.setProperty("r2cloud.apiKey", apiKey);
 		config.setProperty("r2cloud.syncSpectogram", String.valueOf(syncSpectogram));
+		config.setProperty("r2cloud.newLaunches", String.valueOf(newLaunches));
 		config.update();
 		return new Success();
 	}
