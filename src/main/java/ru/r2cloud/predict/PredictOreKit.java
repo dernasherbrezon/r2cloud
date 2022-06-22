6c08d010a376d64fce6029ea0aaec32254a18552
dernasherbrezon <rodionovamp@mail.ru>
2022-06-22 17:43:54 +0100

    temp fix for runtimeexception
    sometimes orekit stop with runtimeexception. not clear why. added logging for troubleshooting

--- src/main/java/ru/r2cloud/predict/PredictOreKit.java
+++ src/main/java/ru/r2cloud/predict/PredictOreKit.java
@@ -40,141 +40,146 @@ import ru.r2cloud.util.Configuration;
 
 public class PredictOreKit {
 
-	public static final double PREDICT_INTERVAL_SECONDS = 3600. * 24 * 2;
-	private static final Logger LOG = LoggerFactory.getLogger(PredictOreKit.class);
-	private static final double SPEED_OF_LIGHT = 2.99792458E8;
+    public static final double PREDICT_INTERVAL_SECONDS = 3600. * 24 * 2;
+    private static final Logger LOG = LoggerFactory.getLogger(PredictOreKit.class);
+    private static final double SPEED_OF_LIGHT = 2.99792458E8;
 
-	private final double minElevation;
-	private final double guaranteedElevation;
-	private final Configuration config;
-	private final Frame earthFrame;
-	private final BodyShape earth;
+    private final double minElevation;
+    private final double guaranteedElevation;
+    private final Configuration config;
+    private final Frame earthFrame;
+    private final BodyShape earth;
 
-	public PredictOreKit(Configuration config) {
-		this.minElevation = config.getDouble("scheduler.elevation.min");
-		this.guaranteedElevation = config.getDouble("scheduler.elevation.guaranteed");
-		this.config = config;
+    public PredictOreKit(Configuration config) {
+        this.minElevation = config.getDouble("scheduler.elevation.min");
+        this.guaranteedElevation = config.getDouble("scheduler.elevation.guaranteed");
+        this.config = config;
 
-		File orekitData = new File(config.getProperty("scheduler.orekit.path"));
-		if (!orekitData.exists()) {
-			LOG.info("orekit master data doesn't exist. downloading now. it might take some time");
-			OreKitDataClient client = new OreKitDataClient(config.getProperties("scheduler.orekit.urls"));
-			try {
-				client.downloadAndSaveTo(orekitData.toPath());
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
-		manager.addProvider(new DirectoryCrawler(orekitData));
+        File orekitData = new File(config.getProperty("scheduler.orekit.path"));
+        if (!orekitData.exists()) {
+            LOG.info("orekit master data doesn't exist. downloading now. it might take some time");
+            OreKitDataClient client = new OreKitDataClient(config.getProperties("scheduler.orekit.urls"));
+            try {
+                client.downloadAndSaveTo(orekitData.toPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+        manager.addProvider(new DirectoryCrawler(orekitData));
 
-		earthFrame = FramesFactory.getITRF(IERSConventions.IERS_2010, true);
-		earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS, Constants.WGS84_EARTH_FLATTENING, earthFrame);
-	}
+        earthFrame = FramesFactory.getITRF(IERSConventions.IERS_2010, true);
+        earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS, Constants.WGS84_EARTH_FLATTENING, earthFrame);
+    }
 
-	public Long getDownlinkFreq(final Long freq, final long utcTimeMillis, TopocentricFrame currentLocation, final TLEPropagator tlePropagator) {
-		AbsoluteDate date = new AbsoluteDate(new Date(utcTimeMillis), TimeScalesFactory.getUTC());
-		PVCoordinates currentState = tlePropagator.getPVCoordinates(date);
-		final double rangeRate = currentLocation.getRangeRate(currentState, tlePropagator.getFrame(), date);
-		return (long) ((double) freq * (SPEED_OF_LIGHT - rangeRate) / SPEED_OF_LIGHT);
-	}
+    public Long getDownlinkFreq(final Long freq, final long utcTimeMillis, TopocentricFrame currentLocation, final TLEPropagator tlePropagator) {
+        AbsoluteDate date = new AbsoluteDate(new Date(utcTimeMillis), TimeScalesFactory.getUTC());
+        PVCoordinates currentState = tlePropagator.getPVCoordinates(date);
+        final double rangeRate = currentLocation.getRangeRate(currentState, tlePropagator.getFrame(), date);
+        return (long) ((double) freq * (SPEED_OF_LIGHT - rangeRate) / SPEED_OF_LIGHT);
+    }
 
-	public Position getSatellitePosition(long utcTimeMillis, TopocentricFrame currentLocation, final TLEPropagator tlePropagator) {
-		AbsoluteDate date = new AbsoluteDate(new Date(utcTimeMillis), TimeScalesFactory.getUTC());
-		PVCoordinates currentState = tlePropagator.getPVCoordinates(date);
-		double azimuth = FastMath.toDegrees(currentLocation.getAzimuth(currentState.getPosition(), tlePropagator.getFrame(), date));
-		double elevation = FastMath.toDegrees(currentLocation.getElevation(currentState.getPosition(), tlePropagator.getFrame(), date));
-		Position result = new Position();
-		result.setAzimuth(azimuth);
-		result.setElevation(elevation);
-		return result;
-	}
+    public Position getSatellitePosition(long utcTimeMillis, TopocentricFrame currentLocation, final TLEPropagator tlePropagator) {
+        AbsoluteDate date = new AbsoluteDate(new Date(utcTimeMillis), TimeScalesFactory.getUTC());
+        PVCoordinates currentState = tlePropagator.getPVCoordinates(date);
+        double azimuth = FastMath.toDegrees(currentLocation.getAzimuth(currentState.getPosition(), tlePropagator.getFrame(), date));
+        double elevation = FastMath.toDegrees(currentLocation.getElevation(currentState.getPosition(), tlePropagator.getFrame(), date));
+        Position result = new Position();
+        result.setAzimuth(azimuth);
+        result.setElevation(elevation);
+        return result;
+    }
 
-	public List<SatPass> calculateSchedule(Date current, TLEPropagator tlePropagator) {
-		List<SatPass> result = new ArrayList<>();
-		TopocentricFrame baseStationFrame = getPosition();
-		if (baseStationFrame == null) {
-			return Collections.emptyList();
-		}
-		AbsoluteDate initialDate = new AbsoluteDate(current, TimeScalesFactory.getUTC());
-		List<AbsoluteDate> max = new ArrayList<>();
-		ElevationExtremumDetector maxDetector = new ElevationExtremumDetector(600, 1, baseStationFrame).withMaxIter(48 * 60).withHandler(new EventHandler<ElevationExtremumDetector>() {
-			@Override
-			public Action eventOccurred(SpacecraftState s, ElevationExtremumDetector detector, boolean increasing) {
-				if (FastMath.toDegrees(detector.getElevation(s)) > guaranteedElevation) {
-					max.add(s.getDate());
-				}
-				return Action.CONTINUE;
-			}
-		});
-		tlePropagator.clearEventsDetectors();
-		tlePropagator.addEventDetector(new EventSlopeFilter<EventDetector>(maxDetector, FilterType.TRIGGER_ONLY_DECREASING_EVENTS));
-		tlePropagator.setSlaveMode();
-		tlePropagator.propagate(initialDate, new AbsoluteDate(initialDate, PREDICT_INTERVAL_SECONDS));
-		for (AbsoluteDate curMax : max) {
-			SatPass cur = findStartEnd(tlePropagator, baseStationFrame, curMax);
-			if (cur != null) {
-				if (cur.getStartMillis() < current.getTime()) {
-					cur.setStart(initialDate);
-				}
-				result.add(cur);
-			}
-		}
+    public List<SatPass> calculateSchedule(Date current, TLEPropagator tlePropagator) {
+        List<SatPass> result = new ArrayList<>();
+        TopocentricFrame baseStationFrame = getPosition();
+        if (baseStationFrame == null) {
+            return Collections.emptyList();
+        }
+        AbsoluteDate initialDate = new AbsoluteDate(current, TimeScalesFactory.getUTC());
+        List<AbsoluteDate> max = new ArrayList<>();
+        ElevationExtremumDetector maxDetector = new ElevationExtremumDetector(600, 1, baseStationFrame).withMaxIter(48 * 60).withHandler(new EventHandler<ElevationExtremumDetector>() {
+            @Override
+            public Action eventOccurred(SpacecraftState s, ElevationExtremumDetector detector, boolean increasing) {
+                if (FastMath.toDegrees(detector.getElevation(s)) > guaranteedElevation) {
+                    max.add(s.getDate());
+                }
+                return Action.CONTINUE;
+            }
+        });
+        tlePropagator.clearEventsDetectors();
+        tlePropagator.addEventDetector(new EventSlopeFilter<EventDetector>(maxDetector, FilterType.TRIGGER_ONLY_DECREASING_EVENTS));
+        tlePropagator.setSlaveMode();
+        try {
+            tlePropagator.propagate(initialDate, new AbsoluteDate(initialDate, PREDICT_INTERVAL_SECONDS));
+        } catch (Exception e) {
+            LOG.error("unable to calculate schedule for {} date: {}", tlePropagator.getTLE().getSatelliteNumber(), initialDate, e);
+            return Collections.emptyList();
+        }
+        for (AbsoluteDate curMax : max) {
+            SatPass cur = findStartEnd(tlePropagator, baseStationFrame, curMax);
+            if (cur != null) {
+                if (cur.getStartMillis() < current.getTime()) {
+                    cur.setStart(initialDate);
+                }
+                result.add(cur);
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public SatPass calculateNext(Date current, TLEPropagator tlePropagator) {
-		TopocentricFrame baseStationFrame = getPosition();
-		if (baseStationFrame == null) {
-			return null;
-		}
-		AbsoluteDate initialDate = new AbsoluteDate(current, TimeScalesFactory.getUTC());
+    public SatPass calculateNext(Date current, TLEPropagator tlePropagator) {
+        TopocentricFrame baseStationFrame = getPosition();
+        if (baseStationFrame == null) {
+            return null;
+        }
+        AbsoluteDate initialDate = new AbsoluteDate(current, TimeScalesFactory.getUTC());
 
-		MaxElevationHandler maxElevationHandler = new MaxElevationHandler(guaranteedElevation);
-		ElevationExtremumDetector maxDetector = new ElevationExtremumDetector(600, 1, baseStationFrame).withMaxIter(48 * 60).withHandler(maxElevationHandler);
-		tlePropagator.clearEventsDetectors();
-		tlePropagator.addEventDetector(new EventSlopeFilter<EventDetector>(maxDetector, FilterType.TRIGGER_ONLY_DECREASING_EVENTS));
-		tlePropagator.setSlaveMode();
-		tlePropagator.propagate(initialDate, new AbsoluteDate(initialDate, PREDICT_INTERVAL_SECONDS));
-		if (maxElevationHandler.getDate() == null) {
-			return null;
-		}
+        MaxElevationHandler maxElevationHandler = new MaxElevationHandler(guaranteedElevation);
+        ElevationExtremumDetector maxDetector = new ElevationExtremumDetector(600, 1, baseStationFrame).withMaxIter(48 * 60).withHandler(maxElevationHandler);
+        tlePropagator.clearEventsDetectors();
+        tlePropagator.addEventDetector(new EventSlopeFilter<EventDetector>(maxDetector, FilterType.TRIGGER_ONLY_DECREASING_EVENTS));
+        tlePropagator.setSlaveMode();
+        tlePropagator.propagate(initialDate, new AbsoluteDate(initialDate, PREDICT_INTERVAL_SECONDS));
+        if (maxElevationHandler.getDate() == null) {
+            return null;
+        }
 
-		return findStartEnd(tlePropagator, baseStationFrame, maxElevationHandler.getDate());
-	}
+        return findStartEnd(tlePropagator, baseStationFrame, maxElevationHandler.getDate());
+    }
 
-	private SatPass findStartEnd(TLEPropagator tlePropagator, TopocentricFrame baseStationFrame, AbsoluteDate maxElevationTime) {
-		MinElevationHandler minElevationHandler = new MinElevationHandler();
-		ElevationDetector boundsDetector = new ElevationDetector(600, 1, baseStationFrame).withConstantElevation(FastMath.toRadians(minElevation)).withHandler(minElevationHandler);
-		tlePropagator.clearEventsDetectors();
-		tlePropagator.addEventDetector(boundsDetector);
-		// 20 mins before and 20 mins later
-		AbsoluteDate startDate = maxElevationTime.shiftedBy(-20 * 60.0);
-		tlePropagator.propagate(startDate, startDate.shiftedBy(40 * 60.0));
+    private SatPass findStartEnd(TLEPropagator tlePropagator, TopocentricFrame baseStationFrame, AbsoluteDate maxElevationTime) {
+        MinElevationHandler minElevationHandler = new MinElevationHandler();
+        ElevationDetector boundsDetector = new ElevationDetector(600, 1, baseStationFrame).withConstantElevation(FastMath.toRadians(minElevation)).withHandler(minElevationHandler);
+        tlePropagator.clearEventsDetectors();
+        tlePropagator.addEventDetector(boundsDetector);
+        // 20 mins before and 20 mins later
+        AbsoluteDate startDate = maxElevationTime.shiftedBy(-20 * 60.0);
+        tlePropagator.propagate(startDate, startDate.shiftedBy(40 * 60.0));
 
-		if (minElevationHandler.getStart() == null || minElevationHandler.getEnd() == null) {
-			return null;
-		}
+        if (minElevationHandler.getStart() == null || minElevationHandler.getEnd() == null) {
+            return null;
+        }
 
-		SatPass result = new SatPass();
-		result.setStart(minElevationHandler.getStart());
-		result.setEnd(minElevationHandler.getEnd());
-		return result;
-	}
+        SatPass result = new SatPass();
+        result.setStart(minElevationHandler.getStart());
+        result.setEnd(minElevationHandler.getEnd());
+        return result;
+    }
 
-	public TopocentricFrame getPosition() {
-		// get the current position
-		Double lat = config.getDouble("locaiton.lat");
-		Double lon = config.getDouble("locaiton.lon");
-		if (lat == null || lon == null) {
-			return null;
-		}
-		return getPosition(new GeodeticPoint(FastMath.toRadians(lat), FastMath.toRadians(lon), 0.0));
-	}
+    public TopocentricFrame getPosition() {
+        // get the current position
+        Double lat = config.getDouble("locaiton.lat");
+        Double lon = config.getDouble("locaiton.lon");
+        if (lat == null || lon == null) {
+            return null;
+        }
+        return getPosition(new GeodeticPoint(FastMath.toRadians(lat), FastMath.toRadians(lon), 0.0));
+    }
 
-	public TopocentricFrame getPosition(GeodeticPoint point) {
-		return new TopocentricFrame(earth, point, "station1");
-	}
+    public TopocentricFrame getPosition(GeodeticPoint point) {
+        return new TopocentricFrame(earth, point, "station1");
+    }
 
 }
