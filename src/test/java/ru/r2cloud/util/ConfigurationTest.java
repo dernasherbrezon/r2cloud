12c27a54fbe0d31787619d1de3d420d846f874d2
dernasherbrezon <rodionovamp@mail.ru>
2022-09-12 08:16:38 +0100

    add unit test

--- src/test/java/ru/r2cloud/util/ConfigurationTest.java
+++ src/test/java/ru/r2cloud/util/ConfigurationTest.java
@@ -1,14 +1,23 @@
 package ru.r2cloud.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.Properties;
 import java.util.Random;
 import java.util.UUID;
 
@@ -105,14 +114,35 @@ public class ConfigurationTest {
 		} catch (Exception e) {
 			// expected
 		}
-		
+
 		fs.removeMock(config.getTempDirectoryPath());
 		fs.removeMock(userParentPath);
 
 		config = new TestConfiguration(tempFolder, fs);
 		assertEquals(lat, config.getProperty("locaiton.lat"));
 	}
-	
+
+	@Test
+	public void testRemoveValueInUserConfigs() throws Exception {
+		File systemSettings = new File(tempFolder.getRoot(), "system.props");
+		String propertyName = UUID.randomUUID().toString();
+		try (OutputStream os = new BufferedOutputStream(new FileOutputStream(systemSettings))) {
+			Properties props = new Properties();
+			props.setProperty(propertyName, "1");
+			props.store(os, "comments");
+		}
+		File userSettings = new File(tempFolder.getRoot(), "user.props");
+		try (OutputStream os = new BufferedOutputStream(new FileOutputStream(userSettings))) {
+			Properties props = new Properties();
+			props.setProperty(propertyName, "");
+			props.store(os, "comments");
+		}
+		try (InputStream is = new BufferedInputStream(new FileInputStream(systemSettings))) {
+			Configuration config = new Configuration(is, userSettings.getAbsolutePath(), fs);
+			assertNull(config.getProperty(propertyName));
+		}
+	}
+
 	@Before
 	public void start() throws Exception {
 		fs = new MockFileSystem(FileSystems.getDefault());
