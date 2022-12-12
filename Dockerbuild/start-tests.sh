930c731ccca0731a155c1b1ce5db539639af1551
dernasherbrezon <dernasherbrezon@gmail.com>
2022-12-12 20:19:51 +0000

    fix test

--- Dockerbuild/start-tests.sh
+++ Dockerbuild/start-tests.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-/home/pi/r2cloud-jdk/bin/java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud-tests/lib/* -Djava.compiler=NONE -Duser.timezone=UTC -Dr2cloud.baseurl=http://localhost:8097 -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.it.IntegrationalTests
+/home/pi/r2cloud-jdk/bin/java -cp /home/pi/r2cloud/etc:/home/pi/r2cloud-tests/lib/* -Djdk.lang.Process.launchMechanism=vfork -Djava.compiler=NONE -Duser.timezone=UTC -Dr2cloud.baseurl=http://localhost:8097 -Djava.util.logging.config.file=/home/pi/r2cloud/etc/logging-prod.properties ru.r2cloud.it.IntegrationalTests
