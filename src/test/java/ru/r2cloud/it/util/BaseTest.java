07088c399b7d73eda094344a00a08e1d5e298129
dernasherbrezon <dernasherbrezon@gmail.com>
2024-02-28 22:28:16 +0000

    fix logging

--- src/test/java/ru/r2cloud/it/util/BaseTest.java
+++ src/test/java/ru/r2cloud/it/util/BaseTest.java
@@ -80,8 +80,7 @@ public abstract class BaseTest {
 
 	@Before
 	public void start() throws Exception {
-		//FIXME uncomment
-//		LogManager.getLogManager().reset();
+		LogManager.getLogManager().reset();
 		tempDirectory = new File(tempFolder.getRoot(), "tmp");
 		if (!tempDirectory.mkdirs()) {
 			throw new RuntimeException("unable to create temp dir: " + tempDirectory.getAbsolutePath());
