a0b8bdaee37d5993e79f41f9b52ea150ac44751e
dernasherbrezon <dernasherbrezon@gmail.com>
2024-11-18 11:32:45 +0000

    fix unit test

--- src/test/java/ru/r2cloud/it/SatelliteLoadTest.java
+++ src/test/java/ru/r2cloud/it/SatelliteLoadTest.java
@@ -1,5 +1,10 @@
 package ru.r2cloud.it;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 import ru.r2cloud.TestUtil;
@@ -8,7 +13,9 @@ import ru.r2cloud.it.util.RegisteredTest;
 public class SatelliteLoadTest extends RegisteredTest {
 
 	@Test
-	public void testLoad() {
+	public void testLoad() throws Exception {
+		Path p = config.getPathFromProperty("satellites.meta.location");
+		Files.setLastModifiedTime(p, FileTime.from(1719525695573L, TimeUnit.MILLISECONDS));
 		TestUtil.assertJson("expected/satellite.json", client.getSatellite("25338"));
 	}
 
