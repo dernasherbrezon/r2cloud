09373d0322123d206a701d97cdb880dfbb155a9d
dernasherbrezon <dernasherbrezon@gmail.com>
2025-04-12 21:40:21 +0100

    fix missing modulation when enabling satellite
    + better unit tests

--- src/test/java/ru/r2cloud/it/ScheduleSaveTest.java
+++ src/test/java/ru/r2cloud/it/ScheduleSaveTest.java
@@ -1,17 +1,13 @@
 package ru.r2cloud.it;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 import java.net.http.HttpResponse;
 import java.util.UUID;
 
 import org.junit.Test;
 
-import com.eclipsesource.json.JsonObject;
-
+import ru.r2cloud.TestUtil;
 import ru.r2cloud.it.util.RegisteredTest;
 
 public class ScheduleSaveTest extends RegisteredTest {
@@ -21,11 +17,8 @@ public class ScheduleSaveTest extends RegisteredTest {
 		// PEGASUS not enabled by default
 		String satelliteId = "42784";
 		client.updateSchedule(satelliteId, true);
-		JsonObject result = client.updateSchedule(satelliteId, true);
-		assertNotNull(result.get("nextPass"));
-		assertTrue(result.getBoolean("enabled", false));
-		result = client.updateSchedule(satelliteId, false);
-		assertNull(result.get("nextPass"));
+		TestUtil.assertJson("expected/enableSatelliteResponse.json", client.updateSchedule(satelliteId, true));
+		TestUtil.assertJson("expected/disableSatelliteResponse.json", client.updateSchedule(satelliteId, false));
 	}
 
 	@Test
