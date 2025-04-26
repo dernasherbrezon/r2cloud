6678c5679c6fdf3318593680c8629867b2708292
dernasherbrezon <dernasherbrezon@gmail.com>
2025-04-26 18:14:13 +0200

    add test for .cadu convertion logic

--- src/test/java/ru/r2cloud/satellite/decoder/SatdumpDecoderTest.java
+++ src/test/java/ru/r2cloud/satellite/decoder/SatdumpDecoderTest.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -18,6 +22,8 @@ import org.junit.rules.TemporaryFolder;
 
 import ru.r2cloud.TestConfiguration;
 import ru.r2cloud.TestUtil;
+import ru.r2cloud.jradio.lrpt.LRPTInputStream;
+import ru.r2cloud.jradio.lrpt.Vcdu;
 import ru.r2cloud.model.DataFormat;
 import ru.r2cloud.model.DecoderResult;
 import ru.r2cloud.model.Instrument;
@@ -36,6 +42,34 @@ public class SatdumpDecoderTest {
 
 	private SatdumpDecoder decoder;
 	private SatelliteDao satelliteDao;
+	private TestConfiguration config;
+
+	@Test
+	public void testMeteor() throws Exception {
+		TestUtil.copyFolder(new File("./src/test/resources/satdump_meteor/").toPath(), tempFolder.getRoot().toPath());
+		File raw = new File(tempFolder.getRoot(), "output.raw");
+		config.setProperty("satellites.meta.location", "./src/test/resources/satellites-meteor.json");
+		satelliteDao = new SatelliteDao(config);
+		
+		Observation req = new Observation();
+		req.setId(UUID.randomUUID().toString());
+		req.setSampleRate(288_000);
+		req.setDataFormat(DataFormat.COMPLEX_UNSIGNED_BYTE);
+		Satellite meteormn23 = satelliteDao.findById("57166");
+		Transmitter lrpt = meteormn23.getById("57166-0");
+
+		DecoderResult result = decoder.decode(raw, req, lrpt, meteormn23);
+		assertNotNull(result.getInstruments());
+		assertEquals(1, result.getInstruments().size());
+		assertNotNull(result.getData());
+		List<Vcdu> data = new ArrayList<>();
+		try (LRPTInputStream is = new LRPTInputStream(new BufferedInputStream(new FileInputStream(result.getData())))) {
+			while (is.hasNext()) {
+				data.add(is.next());
+			}
+		}
+		assertEquals(2, data.size());
+	}
 
 	@Test
 	public void testSuccess() throws Exception {
@@ -81,7 +115,7 @@ public class SatdumpDecoderTest {
 		mocks.put(satdump, new ProcessWrapperMock(new ByteArrayInputStream(new byte[0]), new ByteArrayOutputStream(), new ByteArrayInputStream(new byte[0]), 0, false));
 		ProcessFactoryMock processFactory = new ProcessFactoryMock(mocks, UUID.randomUUID().toString());
 
-		TestConfiguration config = new TestConfiguration(tempFolder);
+		config = new TestConfiguration(tempFolder);
 		config.setProperty("server.tmp.directory", tempFolder.getRoot().getAbsolutePath());
 		config.setProperty("satellites.satdump.path", satdump);
 		config.setProperty("satellites.meta.location", "./src/test/resources/satellites-noaa.json");
