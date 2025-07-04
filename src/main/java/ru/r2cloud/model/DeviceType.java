ed10e26c3b86cdbb77968e8d812b2c5cb20592d6
dernasherbrezon <dernasherbrezon@gmail.com>
2025-07-04 18:36:49 +0100

    remove unused

--- src/main/java/ru/r2cloud/model/DeviceType.java
+++ src/main/java/ru/r2cloud/model/DeviceType.java
@@ -2,17 +2,6 @@ package ru.r2cloud.model;
 
 public enum DeviceType {
 
-	RTLSDR("rtlsdr"), PLUTOSDR("plutosdr"), LORAAT(null), LORAATBLE(null), LORAATBLEC(null), LORAATWIFI(null), SDRSERVER(null), SPYSERVER("spyserver"), AIRSPY("airspy");
-
-	private final String satdumpCode;
-
-	private DeviceType(String satdumpCode) {
-		this.satdumpCode = satdumpCode;
-	}
-
-	// FIXME remove. not used anymore
-	public String getSatdumpCode() {
-		return satdumpCode;
-	}
+	RTLSDR, PLUTOSDR, LORAAT, LORAATBLE, LORAATBLEC, LORAATWIFI, SDRSERVER, SPYSERVER, AIRSPY;
 
 }
